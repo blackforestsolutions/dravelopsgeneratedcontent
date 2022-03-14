f1b42a2c9fd774e5728bad00e36308ddb72f45c3
hennihaus <jan-hendrik.hausner@outlook.com>
2022-03-14 14:43:15 +0100

    added stats for gtfs as constants #minor

---
+++ src/test/java/de/blackforestsolutions/dravelopsgeneratedcontent/testutils/GtfsUtils.java
@@ -0,0 +1,16 @@
+package de.blackforestsolutions.dravelopsgeneratedcontent.testutils;
+
+public class GtfsUtils {
+
+    public static final int RNV_GTFS_STOPS_SIZE = 2384;
+    public static final int RNV_GTFS_TRIPS_SIZE = 14991;
+    public static final int RNV_GTFS_STOP_TIMES_SIZE = 295628;
+
+    public static final int SBG_GTFS_STOPS_SIZE = 3838;
+    public static final int SBG_GTFS_TRIPS_SIZE = 22868;
+    public static final int SBG_GTFS_STOP_TIMES_SIZE = 375921;
+
+    public static final int ALL_GTFS_STOPS_SIZE = RNV_GTFS_STOPS_SIZE + SBG_GTFS_STOPS_SIZE;
+    public static final int ALL_GTFS_TRIPS_SIZE = RNV_GTFS_TRIPS_SIZE + SBG_GTFS_TRIPS_SIZE;
+    public static final int ALL_GTFS_STOP_TIMES_SIZE = RNV_GTFS_STOP_TIMES_SIZE + SBG_GTFS_STOP_TIMES_SIZE;
+}
